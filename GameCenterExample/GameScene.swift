7e495d5af86f3219ce239a1dbb19d35422066690
Davi Chaves <dchaves@usc.edu>
2016-08-11 16:27:25 -0700

    initial commit

--- GameCenterExample/GameScene.swift
+++ GameCenterExample/GameScene.swift
@@ -9,34 +9,40 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    /* UI Connections */
+    var increaseButton: MSButtonNode!
+    var decreaseButton: MSButtonNode!
+    var submitButton: MSButtonNode!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        /* Set UI connections */
+        increaseButton = self.childNodeWithName("backButton") as! MSButtonNode
+        
+        /* Setup restart button selection handler */
+        increaseButton.selectedHandler = {
+            
+        }
+        
+        /* Set UI connections */
+        decreaseButton = self.childNodeWithName("backButton") as! MSButtonNode
+        
+        /* Setup restart button selection handler */
+        decreaseButton.selectedHandler = {
+            
+        }
+        
+        submitButton = self.childNodeWithName("backButton") as! MSButtonNode
+        
+        submitButton.selectedHandler = {
+            
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
